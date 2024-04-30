d08f8f3cc88eaa2dfceef843615176b0e0def66f
Dasa <dasa.podkulova@gmail.com>
2024-04-30 17:44:42 +0200

    Functional Decomposition

---
+++ src/functionalDecomposition/calculator/SimpleCalculator.java
@@ -0,0 +1,40 @@
+package src.functionalDecomposition.calculator;
+
+public class SimpleCalculator {
+
+    // Implement your methods here
+    public static void subtractTwoNumbers(long a, long b) {
+        System.out.println(a - b);
+    }
+
+    public static void sumTwoNumbers(long a, long b) {
+        System.out.println(a + b);
+    }
+
+    public static void divideTwoNumbers(long a, long b) {
+        if (b == 0) {
+            System.out.println("Division by 0!");
+        } else {
+            System.out.println(a / b);
+        }
+    }
+
+    public static void multiplyTwoNumbers(long a, long b) {
+        System.out.println(a * b);
+    }
+
+    // Implemented method
+    public static void power(long n, long p) {
+        long number = n;
+        long power = p;
+        long result = 1;
+        while (power > 0) {
+            if (power % 2 != 0) {
+                result *= number;
+            }
+            power /= 2;
+            number *= number;
+        }
+        System.out.println(result);
+    }
+}
---
+++ src/functionalDecomposition/katesFunction/Main.java
@@ -0,0 +1,22 @@
+package src.functionalDecomposition.katesFunction;
+
+import java.util.Scanner;
+
+/*
+The template for this function is defined below. Let's decompose it!
+
+Your task is to create three additional methods f1, f2, and f3 for
+each case and complete the method f. Each method should accept x as an argument with double type.*/
+public class Main {
+    public static void main(String[] args) {
+
+        MultipleFunction multipleFunction = new MultipleFunction();
+
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+
+        System.out.println(MultipleFunction.f(x));
+
+    }
+}
